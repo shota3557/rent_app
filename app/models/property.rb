2a7b5017565f7232fa2a226017a9b5288019b319
shota kikuchi <shota3557@gmail.com>
2022-10-23 15:24:11 +0900

    accepts_nested_attributes_forメソッドを定義した

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
-  has_many :near_stations
+  has_many :near_stations, dependent: :destroy, inverse_of: :property
   validates :name, :rent, :address, :year, :remarks, presence: true
+  accepts_nested_attributes_for :near_stations, allow_destroy: true, reject_if: :all_blank
 end
